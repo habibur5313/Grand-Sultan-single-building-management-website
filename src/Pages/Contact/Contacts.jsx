8c4a29f7760c9518735eb8fa31b8e27a15d04c52
habibur5313 <habiburmamun313@gmail.com>
2025-02-19 15:57:25 +0600

    contact page title added

--- src/Pages/Contact/Contacts.jsx
+++ src/Pages/Contact/Contacts.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import LocationMap from '../Home/locationMap';
-import Contact from '../Home/Contact';
+import React, { useEffect } from "react";
+import LocationMap from "../Home/locationMap";
+import Contact from "../Home/Contact";
 
 const Contacts = () => {
-                    return (
-                                        <div>
-                                        <LocationMap></LocationMap>  
-                                        <Contact></Contact>                  
-                                        </div>
-                    );
+  useEffect(() => {
+    document.title = "Contacts | Grand Sultan ";
+  }, []);
+  return (
+    <div>
+      <LocationMap></LocationMap>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default Contacts;
